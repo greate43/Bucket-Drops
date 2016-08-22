a6f2549a348b58fd57a944ac1d7a12b2314f108f
Salman Khan <greate43.sk@gmail.com>
2016-08-22 20:38:41 +0500

    realm database working now

--- Bucketdrops/app/src/main/java/sk/greate43/bucketdrops/MainActivity.java
+++ Bucketdrops/app/src/main/java/sk/greate43/bucketdrops/MainActivity.java
@@ -2,12 +2,41 @@ package sk.greate43.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initImageView();
+
     }
+
+
+    private void initImageView() {
+        ImageView background=(ImageView)findViewById(R.id.background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
+    }
+
+    public void addBtn(View view){
+         showDialogAdd();
+    }
+
+    private void showDialogAdd() {
+        addDialog dialog=new addDialog();
+        dialog.show(getSupportFragmentManager(),"Add");
+
+    }
+
 }
